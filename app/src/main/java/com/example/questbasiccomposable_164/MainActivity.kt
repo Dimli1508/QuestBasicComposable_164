3a5f6925b66dc3e128aa51feab031ef44a2f7888
Mashell0707 <merlsutama@gmail.com>
2024-10-19 01:40:01 +0700

    memunculkan logo umy dengan shape bulat

--- app/src/main/java/com/example/questbasiccomposable_164/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_164/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,8 +55,13 @@ fun Dimas (modifier: Modifier = Modifier) {
 
         Text("Ini Adalah Halaman Login",
             fontSize = 30.sp,
-            fontWeight = FontWeight.Thin)
+            fontWeight = FontWeight.Medium)
 
+        Image(
+            painter = painterResource(id = R.drawable.umy),
+            contentDescription = "",
+            modifier = Modifier.clip(CircleShape)
+        )
 
     }
 }
