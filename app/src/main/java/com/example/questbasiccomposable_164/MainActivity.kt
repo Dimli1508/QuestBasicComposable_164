196638cf1078011de61559bc452f9618ad6f2a9c
Mashell0707 <merlsutama@gmail.com>
2024-10-19 01:21:21 +0700

    membuat fun Dimas

--- app/src/main/java/com/example/questbasiccomposable_164/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_164/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questbasiccomposable_164.ui.theme.QuestBasicComposable_164Theme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,13 +23,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestBasicComposable_164Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    Dimas(modifier = Modifier.padding(innerPadding))
                     )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Dimas (modifier: Modifier = Modifier) {
+    Column {
+        HorizontalAlignment = Alignment.CenterHorizontally,
+
     }
 }
 
