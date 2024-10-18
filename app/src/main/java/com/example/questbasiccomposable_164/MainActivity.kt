9e837c59e2284a66a3aa90815dca75768e968b65
Mashell0707 <merlsutama@gmail.com>
2024-10-19 01:23:48 +0700

    mengatur urutan tulisan mulai dari atas layar

--- app/src/main/java/com/example/questbasiccomposable_164/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_164/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questbasiccomposable_164.ui.theme.QuestBasicComposable_164Theme
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
@@ -34,7 +36,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Dimas (modifier: Modifier = Modifier) {
     Column {
-        HorizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlingment = Alignment.CenterHorizontally,
+        verticalArrangment =Arrangement.Top,
 
     }
 }
